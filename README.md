c00012e499d483eb9921ab94f3f6e3fe33f06d55
Adam Wheeler <a.wheeler@columbia.edu>
2018-08-22 12:27:49 -0400

    improve README

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-Hello, world!
+This code implements the algorithm in [|Wheeler & Kipping 201?].  Find posterior samples from the paper at [|github link tk].
+
+This code is tested with Julia 1.0, but it should be compatible with 0.6 as well.  It requires the `Interpolations`, `DataFrames`, and `FITSIO` packages.  To install them run these commands on the Julia REPL
+
+    Pkg.add("Interpolations")
+    Pkg.add("DataFrames")
+    Pkg.add("FITSIO")
+
+Here is a breif usage example
+
+    include("WeirdDetector")
+    using WeirdDetector #make key functions accessible without being qualified
+    
+    #here's how you can get a Kepler light curve detrended like we did in the paper
+    getFITS(8462852, fitsdir="fitsdir/") #download FITS files for Boyajian's star
+    df = loadFITS(8462852) #load all quarters into single data frame, perform ourlier rejection and detrending
+    data = pointsify(df) #convert to data type taken by periodogram()
+
+
+    #Here's how to prep arbitary data
+    t = ...      # array of epochs
+    F = ...      # array of flux values
+    sigmaF = ... # array of 1-sigma uncertainties
+    data = Point.(t, F, sigmaF) #this constructs an array of Points with Julia's broadcast syntax
+
--- WeirdDetector.jl
+++ WeirdDetector.jl
@@ -2,6 +2,7 @@ module WeirdDetector
 
 using Interpolations
 using DataFrames
+using FITSIO
 
 export Point, periodogram, aliases
 
@@ -24,14 +25,14 @@ end
 
 "helper function for smoothing"
 function access(data::Vector{A}, i::Int) :: A where A
-    const npoints = length(data)
+    npoints = length(data)
     j = mod(i,npoints) == 0 ? npoints : mod(i,npoints)
     data[j] 
 end
 
 "helper function for smoothing"
 function wrappedtime(data::Vector{Point}, i::Int, P::Float32) :: Float32
-    const npoints = length(data)
+    npoints = length(data)
     modt = access(data, i).modt
     if i < 1
         return modt - P
@@ -128,7 +129,7 @@ end
 function periodogram(data::Vector{Point}, periods::Vector{Float32}, kw=0.002f0; 
                      parallel=true, datakw=false)
     df = DataFrame(chi2=Float32[], kurtosis=Float32[])
-    const s = div(length(periods), nworkers()*3)
+    s = div(length(periods), nworkers()*3)
     results = pmap(periods, distributed=parallel, batch_size=s) do p
         fold!(p, data)
         row = Vector{Any}()
@@ -208,6 +209,162 @@ function optimal_periods(pmin=0.25f0, pmax=5f1; n=5)
     pmin = Float32(pmin)
     pmax = Float32(pmax)
    exp.((log(pmin) : (0.001f0/n) : log(pmax))) 
+end
+
+"download FITS data for KIC"
+getFITS(KIC::Int; fitsdir="fitsfiles/") = getFITS(lpad(KIC,9,0); fitsdir=fitsdir)
+function getFITS(KIC::String; fitsdir="fitsfiles/", force=false)
+    #info("Dowloading data for KIC $(KIC)")
+    ftpfolder = "http://archive.stsci.edu/pub/kepler/lightcurves//" * KIC[1:4] * "/" * KIC * "/"
+    command = `wget -P $fitsdir -nH --cut-dirs=6 -r -c -N -np -q -R '*.tar' -R 'index*' -erobots=off $ftpfolder`
+    run(command)
+end
+
+#window width is kw*2+1
+"remove outliers from time series.  This is a hleper function for loadFITS"
+function prune(df::DataFrame, threshold_sigma::Int, kw::Int; usephoto=false) :: DataFrame
+    nrows = size(df,1)
+    med = map(1:nrows) do i
+        lb = i-kw < 1 ? 1 : i-kw
+        ub = i+kw < nrows ? i+kw : nrows
+        m = median(df[:F][lb:ub])
+        m, median(abs.(m .- df[:F][lb:ub]))
+    end
+    df = df[1+kw : end-kw, :]
+    med = med[1+kw : end-kw]
+    sigma = (m->m[2]).(med)
+    med = (m->m[1]).(med)
+    if usephoto
+        df[abs.(df[:F] .- med) .< threshold_sigma.*df[:sigmaF], :]
+    else
+        df[abs.(df[:F] .- med) .< threshold_sigma.*sigma.*1.4826, :]
+    end
+end
+
+"detrend with moving median.  This is a helper function for loadFITS"
+function detrend(df::DataFrame, P, trim=true, func=median)
+    m = func(df[:F])
+    df[:F] .-= m
+    fs = 60*24 / 29.4 #sampling frequency [days^-1]
+    kw = Int(round(P*fs/2))
+    nrows = size(df, 1)
+    smoothedF = Vector{Float32}(nrows)
+    for i in 1:nrows
+        lb = i-kw < 1 ? 1 : i-kw
+        ub = i+kw < nrows ? i+kw : nrows
+        smoothedF[i] = func(df[:F][lb:ub])
+    end
+    df[:F] .= df[:F] .+ m .- smoothedF
+    if trim
+        df[1+kw: end-kw, :]
+    else
+        df
+    end
+end
+
+"Interpolate missing points from LC.  This is a helper function for loadFITS"
+function interpolate_missing!(df::DataFrame)
+    #collect points to interpolate
+    df[:interpolated] = false
+    newTimes = Vector{Float64}()
+    dt = 0.020416 #kepler cadence in days
+    epsilon = 0.005
+    for i in 2:size(df,1)
+        t = df[i-1, :t]
+        while df[i,:t] - t > dt + epsilon
+            t += dt
+            push!(newTimes, t)
+        end
+    end
+    itp = Interpolations.interpolate((Vector{Float32}(df[:t]),), 
+                                     Vector{Float32}(df[:F]), Gridded(Linear()))
+    for t in newTimes
+        row = [t, itp[t], missing, true]
+        push!(df, row)
+    end
+    #sort in interpolated points
+    sort!(df, [:t])
+end
+
+"""
+returns a DataFrame containing the lightcurve across all quarters for KIC
+with outliers removed and missing and bad   chi2s = chi2(data, periods)points interpolated.
+"""
+loadFITS(KIC::Int; kwargs...) = loadFITS(lpad(KIC,9,0); kwargs...)
+function loadFITS(KIC::String; usetimecorr=true, prune_kw=5, prune_threshold=5, 
+                  prune_usephoto=false, usephasmaP=0, cadence="llc", 
+                  nodetrend=false, detrend_kw=2, splitwidth=0.3, trim=true, detrend_with=median, 
+                  quarters=1:16, fitsdir="fitsfiles/", usePDC=true) :: DataFrame
+    filenames = [fitsdir*fn for fn in readdir(fitsdir) 
+                 if contains(fn, KIC) && contains(fn, cadence)] 
+    if filenames == []
+        return DataFrame()
+    end
+    dfs = []
+    for fn in filenames
+        f = FITS(fn)
+        quarter = read_key(f[1], "QUARTER")[1]
+        if quarter in quarters
+            df = DataFrame()
+            if usePDC
+                fluxcol = "PDCSAP_FLUX"
+            else
+                fluxcol = "SAP_FLUX"
+            end
+            names = [("TIME", :t), ("TIMECORR", :tcorr), (fluxcol , :F), 
+                     ("PDCSAP_FLUX_ERR", :sigmaF), ("SAP_QUALITY", :QUALITY)]
+            for (oldname, newname) in names
+                df[newname] = [isnan(x) ? missing : x for x in read(f[2], oldname)]
+            end 
+            if usetimecorr
+                df[:t] .+= df[:tcorr]
+            end
+            delete!(df, :tcorr)
+            dropmissing!(df)
+            #remove outliers
+            df = prune(df, prune_threshold, prune_kw, usephoto=prune_usephoto)
+            #drop bad points
+            df = df[df[:QUALITY] .== 0,:]
+            delete!(df, :QUALITY)
+            ts = df[:t]
+            delts = [ts[i+1] - ts[i] for i in 1:(length(ts)-1)]            
+            boundaries = vcat([0], find((dt->dt>splitwidth), delts), [length(ts)])
+            if boundaries == [0,0]
+                continue
+            end
+            for i in 1:(length(boundaries)-1)
+                sdf = df[boundaries[i]+1:boundaries[i+1], :]
+                if sdf[end, :t] - sdf[1, :t] < splitwidth
+                    continue
+                end
+                push!(dfs, sdf)
+            end
+        end
+        close(f)
+    end
+    #detrend and normalized each segment
+    for i in 1:length(dfs)
+        dfs[i][:F] = convert(Vector{Float32}, dfs[i][:F])
+        interpolate_missing!(dfs[i])
+        if usephasmaP != 0
+            dfs[i] = phasma(dfs[i], usephasmaP)
+        elseif !nodetrend
+            dfs[i] = detrend(dfs[i], detrend_kw, trim, detrend_with)
+        end
+        #normalized each quarter individually
+        m = mean(dfs[i][:F])
+        dfs[i][:F] ./= m
+        dfs[i][:sigmaF] ./= m
+        dfs[i][:F] .-= 1
+    end
+    if length(dfs) == 0
+        return DataFrame()
+    elseif length(dfs) == 1
+        return dfs[1]
+    else
+        #combine quarters
+        return vcat(dfs...)
+    end
 end
 
 end
